a2a5ee007ebf9c74e9f89f7380f0213b26f19519
Valery-Lyceum <155839204+Valery-Lyceum@users.noreply.github.com>
2024-04-15 16:49:55 +0200

    Add files via upload

---
+++ mainB.py
@@ -0,0 +1,72 @@
+# Импортируем необходимые классы.
+import logging
+import datetime as dt
+from telegram.ext import Application, MessageHandler, filters, CommandHandler
+
+# Запускаем логгирование
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+
+def remove_job_if_exists(name, context):
+    """Удаляем задачу по имени.
+    Возвращаем True если задача была успешно удалена."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+TIMER = 5
+# Обычный обработчик, как и те, которыми мы пользовались раньше.
+async def set_timer(update, context):
+    """Добавляем задачу в очередь"""
+    chat_id = update.effective_message.chat_id
+    # Добавляем задачу в очередь
+    # и останавливаем предыдущую (если она была)
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    context.job_queue.run_once(task, TIMER, chat_id=chat_id, name=str(chat_id), data=TIMER)
+
+    text = f'Вернусь через 5 с.!'
+    if job_removed:
+        text += ' Старая задача удалена.'
+    await update.effective_message.reply_text(text)
+
+
+async def task(context):
+    """Выводит сообщение"""
+    await context.bot.send_message(context.job.chat_id, text=f'КУКУ! 5c. прошли!')
+
+
+async def unset(update, context):
+    """Удаляет задачу, если пользователь передумал"""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = 'Таймер отменен!' if job_removed else 'У вас нет активных таймеров'
+    await update.message.reply_text(text)
+
+
+def main():
+    # Создаём объект Application.
+    # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
+    application = Application.builder().token("TOKEN").build()
+
+    # Создаём обработчик сообщений типа filters.TEXT
+    # из описанной выше асинхронной функции echo()
+    # После регистрации обработчика в приложении
+    # эта асинхронная функция будет вызываться при получении сообщения
+    # с типом "текст", т. е. текстовых сообщений.
+
+    # Регистрируем обработчик в приложении.
+    application.add_handler(CommandHandler("set", set_timer))
+    application.add_handler(CommandHandler("unset", unset))
+    # Запускаем приложение.
+    application.run_polling()
+
+
+# Запускаем функцию main() в случае запуска скрипта.
+if __name__ == '__main__':
+    main()
