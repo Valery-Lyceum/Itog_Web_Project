285c5a7195de42fa9d5383157f191134097f675b
Valery-Lyceum <vsdulov2007@gmail.com>
2024-04-29 22:03:59 +0300

    Обработал исключения, добавил таймер, сделал косметические правки

--- mainBOT.py
+++ mainBOT.py
@@ -1,7 +1,6 @@
 import logging
 import math
 import aiohttp
-import datetime
 from data.users import User
 import mainDB
 from LxmlSoup import LxmlSoup
@@ -61,7 +60,7 @@ async def get_response(url):
 async def subject(update, context):
     reply_keyboard = [["math"], ["phisics"], ["informatics"]]
     await update.message.reply_text(
-        "Select the subject you want to learn about",
+        "Выберите предмет",
         reply_markup=ReplyKeyboardMarkup(
             reply_keyboard, one_time_keyboard=True, input_field_placeholder="Send subject"
         ),
@@ -73,7 +72,7 @@ async def studing_class(update, context):
     context.user_data['subject'] = update.message.text
     reply_keyboard = [["8"], ["9"], ["10"], ["11"]]
     await update.message.reply_text(
-        "Select your trainig class",
+        "Выберите класс обучения",
         reply_markup=ReplyKeyboardMarkup(
             reply_keyboard, one_time_keyboard=True, input_field_placeholder="Send class"
         ),
@@ -84,7 +83,7 @@ async def studing_class(update, context):
 async def action(update, context):
     reply_keyboard = [["add"], ["del"], ["see"]]
     await update.message.reply_text(
-        "Select action",
+        "Выберите действие",
         reply_markup=ReplyKeyboardMarkup(
             reply_keyboard, one_time_keyboard=True, input_field_placeholder="Send action"
         ),
@@ -94,18 +93,25 @@ async def action(update, context):
 
 async def needs(update, context):
     context.user_data["need"] = update.message.text
-    need = context.user_data["need"] = update.message.text
+    need = context.user_data["need"]
     if need == "see":
+        reply_keyboard = [["Выполнить запрос"]]
+        await update.message.reply_text(
+            "Подтвердите запрос",
+            reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True
+            ),
+        )
         return NAME
     elif need == "del":
         await update.message.reply_text(
-            "enter name olimpiads",
+            "Введите название олимпиады",
             reply_markup=ReplyKeyboardRemove()
             )
         return NAMEPOINTS
     elif need == "add":
         await update.message.reply_text(
-            "enter name olimpiads and points for it",
+            "Введите название олимпиады и баллы за нее",
             reply_markup=ReplyKeyboardRemove()
             )
         return NAMEPOINTS
@@ -113,12 +119,6 @@ async def needs(update, context):
 
 async def see(update, context):
     cash = mainDB.nomain(context.user_data["need"], update.message.from_user.id)
-    """
-    f = open(f"{datetime}", 'w')
-    for i in cash:
-        print(f"{i[0]}: {i[1]}", file=f)
-    f.close()
-    """
     vivod = ''
     for i in cash:
         vivod += f"{i[0]}: {i[1]}\t"
@@ -133,18 +133,27 @@ async def see(update, context):
 async def add_del(update, context):
     nazv = update.message.text
     need = context.user_data["need"]
-    if need == "add":
-        t = nazv.split()
-        l = len(t)
-        nazv, pt = ' '.join(t[:l-1]), int(t[-1])
-        mainDB.nomain(context.user_data["need"], update.message.from_user.id, nazv, pt)
-    elif need == "del":
-        mainDB.nomain(context.user_data["need"], update.message.from_user.id, nazv)
+    try:
+        if need == "add":
+            t = nazv.split()
+            l = len(t)
+            nazv, pt = ' '.join(t[:l-1]), int(t[-1])
+            mainDB.nomain(context.user_data["need"], update.message.from_user.id, nazv, pt)
+        elif need == "del":
+            mainDB.nomain(context.user_data["need"], update.message.from_user.id, nazv)
+        await update.message.reply_text(
+            "Изменения приняты"
+        )
+    except:
+        await update.message.reply_text(
+            "Ошибка!"
+        )
+    return ConversationHandler.END
 
 
 async def entr_adress(update, context):
     await update.message.reply_text(
-        "enter your adress",
+        "Введите адрес начальной точки",
         reply_markup=ReplyKeyboardRemove()
     )
     return UADRESS
@@ -154,7 +163,7 @@ async def u_adress(update, context):
     u_adrs = update.message.text
     context.user_data['u_adress'] = u_adrs
     await update.message.reply_text(
-        "enter object adress"
+        "Введите адрес конечной точки"
     )
     return OADRESS
 
@@ -162,6 +171,13 @@ async def u_adress(update, context):
 async def obj_adress(update, context):
     obj_adrs = update.message.text
     context.user_data['o_adress'] = obj_adrs
+    reply_keyboard = [["Выполнить запрос"]]
+    await update.message.reply_text(
+        "Подтвердите запрос",
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True
+        ),
+    )
     return GEO
 
 
@@ -174,6 +190,10 @@ async def geocoder(update, context):
         "format": "json"}
     response = await get_response2(geocoder_api_server, params=geocoder_params)
     if not response:
+        await update.message.reply_text(
+            "Плохое соединение с сервером",
+            reply_markup=ReplyKeyboardRemove()
+        )
         return ConversationHandler.END
     toponym = response["response"]["GeoObjectCollection"][
         "featureMember"][0]["GeoObject"]
@@ -188,6 +208,10 @@ async def geocoder(update, context):
         "format": "json"}
     response = await get_response2(geocoder_api_server, params=geocoder_params)
     if not response:
+        await update.message.reply_text(
+            "Плохое соединение с сервером",
+            reply_markup=ReplyKeyboardRemove()
+        )
         return ConversationHandler.END
     toponym = response["response"]["GeoObjectCollection"][
         "featureMember"][0]["GeoObject"]
@@ -207,9 +231,17 @@ async def geocoder(update, context):
     await context.bot.send_photo(
         update.message.chat_id,
         static_api_request,
-        caption=lonlat_distance([float(toponym_longitude), float(toponym_lattitude)], [float(toponym_longitude2), float(toponym_lattitude2)])
+        caption=lonlat_distance([float(toponym_longitude), float(toponym_lattitude)], [float(toponym_longitude2), float(toponym_lattitude2)]),
+        reply_markup=ReplyKeyboardRemove()
     )
     return ConversationHandler.END
+
+
+async def get_response2(url, params):
+    logger.info(f"getting {url}")
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params) as resp:
+            return await resp.json()
 
 
 def lonlat_distance(a, b):
@@ -221,20 +253,56 @@ def lonlat_distance(a, b):
     dx = abs(a_lon - b_lon) * degree_to_meters_factor * lat_lon_factor
     dy = abs(a_lat - b_lat) * degree_to_meters_factor
     distance = math.sqrt(dx * dx + dy * dy)
-    return str(round(distance)) + " m"
+    return str(round(distance)) + " м"
 
 
-async def get_response2(url, params):
-    logger.info(f"getting {url}")
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params) as resp:
-            return await resp.json()
+async def alarm(context):
+    job = context.job
+    await context.bot.send_message(job.chat_id, text=f"Биип! {job.data} секунд прошли!")
 
 
-async def start(update, context):
+def remove_job_if_exists(name, context):
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+async def set_timer(update, context) -> None:
+    chat_id = update.effective_message.chat_id
+    try:
+        due = float(context.args[0])
+        if due < 0:
+            await update.effective_message.reply_text("К сожалению, мы не можем вернуться в будущее!")
+            return
+
+        job_removed = remove_job_if_exists(str(chat_id), context)
+        context.job_queue.run_once(alarm, due, chat_id=chat_id, name=str(chat_id), data=due)
+
+        text = "Таймер успешно установлен!"
+        if job_removed:
+            text += " Старый удалили."
+        await update.effective_message.reply_text(text)
+    except (IndexError, ValueError):
+        await update.effective_message.reply_text("Используйте: /set <seconds>")
+
+
+async def unset(update, context):
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    if job_removed:
+        text = "Таймер успешно отменен!"
+    else:
+        text = "У вас нет активного таймера."
+    await update.message.reply_text(text)
+
+
+async def menu(update, context):
     reply_keyboard = [["olimps", 'geo'], ["results"]]
     await update.message.reply_text(
-        "Я бот-справочник. Какая информация вам нужна?",
+        "Какая информация вам нужна?",
         reply_markup=ReplyKeyboardMarkup(
             reply_keyboard, one_time_keyboard=True, input_field_placeholder="Send those you want"
         ),
@@ -242,34 +310,46 @@ async def start(update, context):
     return START
 
 
-async def close_keyboard(update, context):
+async def start(update, context):
     await update.message.reply_text(
-        "Ok",
-        reply_markup=ReplyKeyboardRemove()
+        """Здравствуйте! Я бот-помощник для олимпиадника.
+           У меня есть следуюший функционал:
+           /menu - вызывает меню с тремя ветками:
+           1) Выдает список и расписание олимпиад.
+           2) Показывает маршрут и расстояние до интересующего вас кружка.
+           3) Позволяет записать, удалить или посмотреть результаты олимпиады.
+           /set <seconds> - создает таймер для контроля выполнения задач.
+           /unset - удаляет текущий таймер.
+        """
     )
-    return ConversationHandler.END
 
 
 if __name__ == '__main__':
     application = Application.builder().token("TOKEN").build()
+
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("set", set_timer))
+    application.add_handler(CommandHandler("unset", unset))
+
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
+        entry_points=[CommandHandler('menu', menu)],
         states={
             START: [MessageHandler(filters.Regex("^(olimps)$"), subject),
                     MessageHandler(filters.Regex("^(results)$"), action),
                     MessageHandler(filters.Regex("^(geo)$"), entr_adress)],
+
             CLASS: [MessageHandler(filters.Regex("^(math|phisics|informatics)$"), studing_class)],
             OLIMP: [MessageHandler(filters.Regex("^(8|9|10|11)$"), olimps)],
 
             ACTION: [MessageHandler(filters.Regex("^(add|del|see)$"), needs)],
-            NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, see)],
+            NAME: [MessageHandler(filters.Regex("^(Выполнить запрос)$"), see)],
             NAMEPOINTS: [MessageHandler(filters.TEXT & ~filters.COMMAND, add_del)],
 
             UADRESS: [MessageHandler(filters.TEXT & ~filters.COMMAND, u_adress)],
             OADRESS: [MessageHandler(filters.TEXT & ~filters.COMMAND, obj_adress)],
-            GEO: [MessageHandler(filters.TEXT & ~filters.COMMAND, geocoder)]
+            GEO: [MessageHandler(filters.Regex("^(Выполнить запрос)$"), geocoder)]
         },
-        fallbacks=[CommandHandler('close', close_keyboard)]
+        fallbacks=[CommandHandler('menu', menu)]
     )
     application.add_handler(conv_handler)
     application.run_polling()
